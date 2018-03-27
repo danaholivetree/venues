6f47875ac895b9cc10987725a69c4fcfe5f0de9b
Danah Olivetree <danah.olivetree@gmail.com>
2018-03-27 15:30:49 -0600

    ugh

--- migrations/20180327140044_add_contributed_by.js
+++ migrations/20180327140044_add_contributed_by.js
@@ -1,13 +1,12 @@
-//
-// exports.up = function(knex, Promise) {
-// console.log('should not be running this migration')
-//   return knex.schema.table('venues', t => {
-//     t.integer('contributed_by').references('id').inTable('users');
-//   })
-// };
-//
-// exports.down = function(knex, Promise) {
-//   return knex.schema.table('venues', function(t) {
-//           t.dropColumn('contributed_by');
-//       });
-// };
+
+exports.up = function(knex, Promise) {
+  return knex.schema.table('venues', t => {
+    t.integer('contributed_by').references('id').inTable('users');
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.table('venues', function(t) {
+          t.dropColumn('contributed_by');
+      });
+};
