bd5ccb2ab39bc674ec6e440355ad03426cbb43eb
Danah Olivetree <danah.olivetree@gmail.com>
2018-07-10 13:43:09 -0600

    switched to user access token

--- routes/token.js
+++ routes/token.js
@@ -40,9 +40,10 @@ router.get('/facebook/venues/:qs', (req, res, next) => {
   //use app token (once approved)
   // let auth = `&access_token=${app_id}|${app_secret}`
   //use token stolen from graph api for testing
-  let auth = `&access_token=${process.env.TEMP_TOKEN}`
+  // let auth = `&access_token=${process.env.TEMP_TOKEN}`
   //use user access token
-  // let auth = `&access_token=${req.cookies.user.accessToken}`
+  console.log('req.cookies.user.accessToken ', req.cookies.user.accessToken);
+  let auth = `&access_token=${req.cookies.user.accessToken}`
   request.get({url: uri+qs+auth, json: true}, (error, response, body) => {
     if (!error && response.statusCode === 200) {
       res.send(body)
@@ -63,9 +64,10 @@ router.get('/facebook/bands/:qs', (req, res, next) => {
   //use app token (once approved)
   // let auth = `&access_token=${app_id}|${app_secret}`
   //use token stolen from graph api for testing
-  let auth = `&access_token=${process.env.TEMP_TOKEN}`
+  // let auth = `&access_token=${process.env.TEMP_TOKEN}`
   //use user access token doesn't work either
-  // let auth = `&access_token=${req.cookies.user.accessToken}`
+  console.log('req.cookies.user.accessToken ', req.cookies.user.accessToken);
+  let auth = `&access_token=${req.cookies.user.accessToken}`
   request.get({url: uri+qs+auth, json: true}, (error, response, body) => {
     if (!error && response.statusCode === 200) {
       res.send(body)
